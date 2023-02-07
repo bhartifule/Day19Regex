b0e33f1b1efba3d5c73767fbbda9e2fc29e123c2
Bharti <bharatifule10gmail.com>
2023-02-07 23:49:39 +0530

    Added:FirstName Validation using Regex

---
+++ bridglabz/regex/UserRegistration.java
@@ -0,0 +1,23 @@
+package com.bridglabz.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+	 public  void validFirstName() {
+         Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{3,15}$");
+        Matcher matcher = pattern.matcher("Bharti");
+        if (matcher.matches()){
+            System.out.println("valid");
+        }else {
+            System.out.println("invalid");
+        }
+    }
+public static void main(String[] args) {
+	System.out.println("Welcome To Regular Expression");
+	UserRegistration regex = new UserRegistration();
+     regex.validFirstName();
+     
+ }
+}
+
