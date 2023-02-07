823abff675a31ece45e7d75bd378793f1a777776
Bharti <bharatifule10gmail.com>
2023-02-07 23:54:03 +0530

    Added:LastName Validation using Regex

--- bridglabz/regex/UserRegistration.java
+++ bridglabz/regex/UserRegistration.java
@@ -4,6 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+	//UC-1
 	 public  void validFirstName() {
          Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{3,15}$");
         Matcher matcher = pattern.matcher("Bharti");
@@ -12,12 +13,23 @@ public class UserRegistration {
         }else {
             System.out.println("invalid");
         }
+	 }
+	// UC-2
+        public  void validLastName() {
+            Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{3,15}$");
+            Matcher matcher = pattern.matcher("Fule");
+            if (matcher.matches()){
+                System.out.println("valid");
+            }else {
+                System.out.println("invalid");
+            }
     }
 public static void main(String[] args) {
 	System.out.println("Welcome To Regular Expression");
 	UserRegistration regex = new UserRegistration();
-     regex.validFirstName();
-     
+    regex.validFirstName();
+    regex.validLastName();
+    
  }
 }
 
