1343cdef41c13ef1deee35dd8e2e47bd97a7f10a
bhartifule <116824470+bhartifule@users.noreply.github.com>
2023-06-14 17:34:42 +0530

    UserRegistration.java

--- bridglabz/regex/UserRegistration.java
+++ bridglabz/regex/UserRegistration.java
@@ -3,7 +3,8 @@ package com.bridglabz.regex;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class UserRegistration {
+public class UserRegistration 
+{
 	//UC-1
 	 public  void validFirstName() {
          Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{3,15}$");
